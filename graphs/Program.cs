42dcd68ebb90a08116d464b87c92c340f477d73c
Christoph Witzko <github@christophwitzko.com>
2015-03-16 10:48:30 +0100

    feat(graphs): added adjlist methods

--- graphs/Program.cs
+++ graphs/Program.cs
@@ -4,18 +4,29 @@ using System;
 
 namespace GraphExample {
   public class Program {
-    public static void DFSVisit (int vtx, bool[] visit, int[,] adjmatrix) {
+    public static void DFSVisitMatrix (int vtx, bool[] visit, int[,] adjmatrix) {
       int size = adjmatrix.GetLength(0);
       visit[vtx] = true;
       Console.WriteLine("visit vertex: " + vtx);
       for (int i = 0; i < size; i++) {
         if (!visit[i] && adjmatrix[vtx, i] == 1) {
-          DFSVisit(i, visit, adjmatrix);
+          DFSVisitMatrix(i, visit, adjmatrix);
         }
       }
     }
 
-    public static void BFSVisit (int vtx, bool[] visit, int[,] adjmatrix) {
+    public static void DFSVisitList (int vtx, bool[] visit, LinkedList<int>[] adjlist) {
+      int size = adjlist.Length;
+      visit[vtx] = true;
+      Console.WriteLine("visit vertex: " + vtx);
+      adjlist[vtx].Traverse(adjlist[vtx].First, (val, node) => {
+        if (!visit[val]) {
+          DFSVisitList(val, visit, adjlist);
+        }
+      });
+    }
+
+    public static void BFSVisitMatrix (int vtx, bool[] visit, int[,] adjmatrix) {
       int size = adjmatrix.GetLength(0);
       Queue<int> todo = new Queue<int>();
       todo.Enqueue(vtx);
@@ -29,6 +40,24 @@ namespace GraphExample {
               todo.Enqueue(i);
             }
           }
+        }
+      }
+    }
+
+    public static void BFSVisitList (int vtx, bool[] visit, LinkedList<int>[] adjlist) {
+      int size = adjlist.Length;
+      Queue<int> todo = new Queue<int>();
+      todo.Enqueue(vtx);
+      while(todo.Count > 0) {
+        int curr = todo.Dequeue();
+        if (!visit[curr]) {
+          visit[curr] = true;
+          Console.WriteLine("visit vertex: " + curr);
+          adjlist[curr].Traverse(adjlist[curr].First, (val, node) => {
+            if (!visit[val]) {
+              todo.Enqueue(val);
+            }
+          });
         }
       }
     }
@@ -62,13 +91,19 @@ namespace GraphExample {
         { 0, 0, 0, 0, 1, 0, 0, 0, 1, 0}
       };
       LinkedList<int>[] adjlist = ToAdjList(adjmatrix);
-      Console.WriteLine("Depth First Search:");
+      Console.WriteLine("Depth First Search (Matrix):");
       bool[] visitd = new bool[10];
-      DFSVisit(0, visitd, adjmatrix);
-      Console.WriteLine(new String('-', 20));
-      Console.WriteLine("Breadth First Search:");
+      DFSVisitMatrix(0, visitd, adjmatrix);
+      Console.WriteLine("Depth First Search (List):");
+      bool[] visitdl = new bool[10];
+      DFSVisitList(0, visitdl, adjlist);
+      Console.WriteLine(new String('-', 30));
+      Console.WriteLine("Breadth First Search (Matrix):");
       bool[] visitb = new bool[10];
-      BFSVisit(0, visitb, adjmatrix);
+      BFSVisitMatrix(0, visitb, adjmatrix);
+      Console.WriteLine("Breadth First Search (List):");
+      bool[] visitbl = new bool[10];
+      BFSVisitList(0, visitbl, adjlist);
     }
   }
 }
